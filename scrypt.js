5b22665b21b00c803fb67b8f91ce245a3a79bcca
SageofRaven <153455486+SageofRaven@users.noreply.github.com>
2023-12-17 13:36:33 +0300

    Add files via upload

---
+++ scrypt.js
@@ -0,0 +1,14 @@
+function changeStyle1(){
+    let element1 = document.getElementById("font");
+    element1.style.fontSize = "10px";
+}
+
+function changeStyle2(){
+    let element2 = document.getElementById("font");
+    element2.style.fontSize = "15px";
+}
+
+function changeStyle3(){
+    let element3 = document.getElementById("font");
+    element3.style.fontSize = "20px";
+}
